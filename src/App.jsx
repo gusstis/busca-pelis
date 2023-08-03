2dcf64e98c424282653df3666b5baf7c61eb472d
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-03 20:42:01 -0300

    Rendering mock, almost there...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import './App.css'
+import './App.css';
+import responseMovies from './mocks/with-results.json';
+import withoutResults from './mocks/no-results.json';
 
 function App() {
+
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
 
   return (
     <>
@@ -16,7 +21,24 @@ function App() {
           </form>
         </header>
         <main>
-          Acá salen los results
+          {
+            hasMovies
+              ? (
+                  <ul>
+                    {
+                      movies.map(movie => (
+                        <li key={movie.imdbID} >
+                          <h3> {movie.Title} </h3>
+                          <p> {movie.Year} </p>
+                          <img src={movie.Poster} alt={movie.Title} />
+                        </li>
+                      ))
+                    }
+                  </ul>
+                ) : (
+              <p>No se encontraron pelis para su búsqueda</p>
+            )
+          }
         </main>
       </div>
     </>
