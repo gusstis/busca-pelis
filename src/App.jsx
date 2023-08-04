ee0260fde4549d6ba1235cbbef6bcb299822a485
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-04 17:53:06 -0300

    useRef to avoid validating at the beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import { Movies } from './components/Movies';
 import {useMovies} from './hooks/useMovies'
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 function useSearch() {
   const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
     if (search === '') {
       setError('No se puede buscar una peli vacía')
       return
