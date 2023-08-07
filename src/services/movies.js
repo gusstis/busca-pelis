da816167fb3e4534494489dd294ae767b489747c
Gustavo Aranciba <gusaranciba@gmail.com>
2023-08-07 11:41:53 -0300

    Using ENV_VARs in VITE, watch out

---
+++ src/services/movies.js
@@ -0,0 +1,24 @@
+//import {dotenv} from 'react-dotenv'
+
+const apiKey = import.meta.env.VITE_API_KEY
+
+export const searchMovies = async ({search}) => {
+    if (search === '') return null
+    console.log(apiKey)
+
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`)
+        const json = await response.json()
+
+        const movies = json.Search
+        return movies?.map(movie => ({
+          id: movie.imdbID,
+          title: movie.Title,
+          year: movie.Year,
+          poster: movie.Poster
+        }))
+    } catch (e) {
+        throw new Error('Error searching movies')
+    }
+
+}    
